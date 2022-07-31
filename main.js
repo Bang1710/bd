6657b6371aa3f22546a0bb59fdb8272e331c4601
Bang1710 <87267619+Bang1710@users.noreply.github.com>
2022-07-31 22:04:52 +0700

    version final web

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-$(".hover").mouseleave(
-    function () {
-        $(this).removeClass("hover");
-    }
-);
+const $ = document.querySelector.bind(document);
+
 
 var video = document.querySelector("#player");
 function playvideo() {
     video.play();
+}
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector(".myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
